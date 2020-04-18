28da07b9bf13e3bdce7700342031f9c556bc1f4d
icicle4 <icicle4@icloud.com>
2020-04-18 17:07:20 +0800

    init

--- track_mot15_test.sh
+++ track_mot15_test.sh
@@ -3,7 +3,7 @@
 export Test_MOT15=true
 DATA_DIR="/content"
 DIS_THRESHOLD=1.0
-CHECKPOINTPATH="/content/drive/My Drive/checkpoint_0105.pth.tar"
+CHECKPOINTPATH="/content/drive/My \Drive/checkpoint_0105.pth.tar"
 
 python track.py --test_mot15 $Test_MOT15  \
                 --dis_threshold $DIS_THRESHOLD \
