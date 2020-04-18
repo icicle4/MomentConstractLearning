f53eb7c87049b99f45f4f72007477527621cf807
icicle4 <icicle4@icloud.com>
2020-04-18 20:31:25 +0800

    init

--- lib/moco/loader.py
+++ lib/moco/loader.py
@@ -9,7 +9,7 @@ normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
 augmentations = [
-    transforms.Resize(224),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     normalize
 ]
